5c040b25f1ce9132aed4ed49e0e5e12f26084227
Basker12 <kacperileon@gmail.com>
2022-05-30 16:13:37 +0100

    [Addition] Working on the main page content and made the android logo bounce for some reason.

--- OADigitalWebsite/wwwroot/js/site.js
+++ OADigitalWebsite/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿let androidIcon = document.querySelector('.fa-android');
+
+androidIcon.addEventListener('onmouseOver', onmouseover);
+androidIcon.addEventListener('onmouseOut', onmouseout);
+
+function onmouseOver () {
+    androidIcon.classList.add('fa-bounce');
+}
+function onmouseOut () {
+    androidIcon.classList.remove('fa-bounce')
+}
