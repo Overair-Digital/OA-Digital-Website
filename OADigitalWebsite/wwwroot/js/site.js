0a3cc148f81e04594c500e8335c05bfd34d08ba5
Basker12 <kacperileon@gmail.com>
2022-06-01 20:17:37 +0100

    [Addition] Submit button has different styles applied to it and now im working on email validation.

--- OADigitalWebsite/wwwroot/js/site.js
+++ OADigitalWebsite/wwwroot/js/site.js
@@ -24,3 +24,34 @@ function onMouseOut () {
     appleIcon.classList.remove('fa-beat-fade');
 }
 // Apple Icon
+
+// Email Validation
+let form = document.querySelector('#form');
+let email = document.querySelector('#email');
+let name = document.querySelector('#name');
+let msg = document.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    validate();
+})
+function validate () {
+    const emailVal = email.value.trim();
+    const nameVal = name.value.trim();
+    const msgVal = name.value.trim();
+    
+    if (nameVal === '') {
+        showError(name, "The name field cant be empty.")
+    } else {
+        showSuccess(name)
+    }
+}
+
+function showError (input, message) {
+    
+}
+function showSuccess (input) {
+    
+}
+//     let Regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
