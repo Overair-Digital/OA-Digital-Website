15876f84d0c104b84c402eeaf889193bfcbce57e
Basker12 <kacperileon@gmail.com>
2022-06-03 22:17:35 +0100

    [Addition] Changed the layout of the contact section.

--- OADigitalWebsite/wwwroot/js/site.js
+++ OADigitalWebsite/wwwroot/js/site.js
@@ -1,30 +1,4 @@
 ﻿import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
-let androidIcon = document.querySelector('.fa-android');
-let appleIcon = document.querySelector('.fa-apple');
-
-// Android Icon
-androidIcon.addEventListener('onmouseOver', onmouseover);
-androidIcon.addEventListener('onmouseOut', onmouseout);
-
-function onmouseOver () {
-    androidIcon.classList.add('fa-bounce');
-}
-function onmouseOut () {
-    androidIcon.classList.remove('fa-bounce')
-}
-// Android Icon
-
-// Apple Icon
-appleIcon.addEventListener('onMouseOver', onmouseover);
-appleIcon.addEventListener('onMouseOut', onmouseout);
-
-function onMouseOver () {
-    appleIcon.classList.add('fa-beat-fade');    
-}
-function onMouseOut () {
-    appleIcon.classList.remove('fa-beat-fade');
-}
-// Apple Icon
 
 // Email Validation
 let form = document.querySelector('#form');
